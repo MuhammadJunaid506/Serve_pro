631f7ae7415ae7b92ea761a45706d6d7be8370cd
MuhammadJunaid506 <93211293+MuhammadJunaid506@users.noreply.github.com>
2024-01-20 11:40:57 +0500

    images change

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,20 +1,20 @@
-import Logo from "./files/Logo.png";
-import BannerH from "./files/BannerH.jpg"
-import M1 from './files/IMG-20231221-WA0050.jpg'
-import M2 from './files/IMG-20231221-WA0051.jpg'
-import M3 from './files/IMG-20231221-WA0052.jpg'
-import M4 from './files/IMG-20231221-WA0053.jpg'
-import M5 from './files/IMG-20231221-WA0054.jpg'
-import M6 from './files/IMG-20231221-WA0055.jpg'
-import Banner from './files/food-4935258_1280.jpg'
-import B1 from './files/momos-4935232_1280.jpg'
-import B3 from './files/meat-123668_1280.jpg'
-import B2 from './files/sandwich-2977251_1280.jpg'
-import B4 from './files/pizza-3010062_1280.jpg'
-import B5 from './files/food-712665_1280.jpg'
-import B6 from './files/olive-oil-346997_1280.jpg'
-import B7 from './files/pasta-794464_1280.jpg'
-import B8 from './files/pastry-6780834_1280.jpg'
+import Logo from "./files/Black_and_White_Blog_Logo__1_-removebg-preview.png";
+import BannerH from "./files/pexels-life-of-pix-67468.jpg"
+import M1 from './files/pexels-darya-sannikova-2422414.jpg'
+import M2 from './files/pexels-life-of-pix-67468.jpg'
+import M3 from './files/pexels-wendy-wei-1546039.jpg'
+import M4 from './files/pexels-on-shot-2788792.jpg'
+import M5 from './files/pexels-volkan-vardar-3887985.jpg'
+import M6 from './files/pexels-kaboompics-com-6267.jpg'
+import Banner from './files/pexels-on-shot-2788792.jpg'
+import B1 from './files/pexels-asad-photo-maldives-1449773.jpg'
+import B3 from './files/pexels-life-of-pix-67468.jpg'
+import B2 from './files/pexels-mark-1080696.jpg'
+import B4 from './files/pexels-naim-benjelloun-2291599.jpg'
+import B5 from './files/pexels-pixabay-260922.jpg'
+import B6 from './files/pexels-pixabay-262978.jpg'
+import B7 from './files/pexels-darya-sannikova-2422414.jpg'
+import B8 from './files/pexels-wendy-wei-1546039.jpg'
 
 
 export {Logo,BannerH,M1,M2,M3,M4,M5,M6,Banner,B1,B2,B3,B4,B5,B6,B7,B8}
